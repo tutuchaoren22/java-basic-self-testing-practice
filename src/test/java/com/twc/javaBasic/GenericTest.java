64ed8ad23c3c4cee50c17634a91a1e9acbfe5c4f
duyuqian <809009017@qq.com>
2020-07-27 18:41:54 +0800

    [duyuqian] feat-5: finish some operator of generic

--- src/test/java/com/twc/javaBasic/GenericTest.java
+++ src/test/java/com/twc/javaBasic/GenericTest.java
@@ -6,69 +6,54 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GenericTest {
-  @Test
-  void should_auto_resolve_generic_method() {
-    final String[] words = {"Hello", "Good", "Morning"};
+    @Test
+    void should_auto_resolve_generic_method() {
+        final String[] words = {"Hello", "Good", "Morning"};
+        final String middle = getMiddle(words);
+        assertEquals("Good", middle);
+    }
 
-    // TODO:
-    //  The getMiddle method is a generic method. Now, please call getMiddle method for string
-    //  type.
-    // <--start
-    final String middle = null;
-    // --end-->
+    @Test
+    void should_specify_a_type_restriction_on_typed_parameters() {
+        int minimumInteger = min(new Integer[]{1, 2, 3});
+        double minimumReal = min(new Double[]{1.2, 2.2, -1d});
 
-    assertEquals("Good", middle);
-  }
+        assertEquals(1, minimumInteger);
+        assertEquals(-1d, minimumReal, 1.0E-05);
+    }
 
-  @Test
-  void should_specify_a_type_restriction_on_typed_parameters() {
-    // Hint: please implement the min() method in this class.
-    int minimumInteger = min(new Integer[] {1, 2, 3});
-    double minimumReal = min(new Double[] {1.2, 2.2, -1d});
+    @Test
+    void should_swap() {
+        Pair<String> pair = new Pair<>("Hello", "World");
 
-    assertEquals(1, minimumInteger);
-    assertEquals(-1d, minimumReal, 1.0E-05);
-  }
+        swap(pair);
 
-  @Test
-  void should_swap() {
-    Pair<String> pair = new Pair<>("Hello", "World");
+        assertEquals("World", pair.getFirst());
+        assertEquals("Hello", pair.getSecond());
+    }
 
-    // Hint: please implement swap() method in this class.
-    swap(pair);
+    private static <T> T getMiddle(T[] args) {
+        return args[args.length / 2];
+    }
 
-    assertEquals("World", pair.getFirst());
-    assertEquals("Hello", pair.getSecond());
-  }
+    private static <T extends Number & Comparable<T>> T min(T[] values) {
+        if (values.length == 0) {
+            return null;
+        }
+        T minValue = values[0];
+        for (int i = 1; i < values.length; i++) {
+            if (minValue.compareTo(values[i]) > 0) {
+                minValue = values[i];
+            }
+        }
+        return minValue;
 
-  private static <T> T getMiddle(T[] args) {
-    return args[args.length / 2];
-  }
+    }
 
-  // TODO:
-  //  please implement the following code to pass the test. It should be generic
-  //  after all. The method should only accept `Number` and the number should
-  //  implement `Comparable<T>`.
-  //  You should not change the signature of the function. But you can change
-  //  the declaration of the generic type parameter.
-  // <--start
-  private static <T> T min(T[] values) {
-    throw new RuntimeException("Not implemented");
-  }
-  // --end-->
+    private static <T> void swap(Pair<T> pair) {
+        T t = pair.getFirst();
+        pair.setFirst(pair.getSecond());
+        pair.setSecond(t);
+    }
 
-  // TODO:
-  //  please implement following method to pass the test. The method should be able
-  //  to swap fields in a pair. But you cannot change the signature of the function.
-  //
-  // Hint:
-  //  A wildcard is not a type variable, so we can’t write code that uses ? as a type.
-  // <--start
-  private static void swap(Pair<?> pair) {
-    throw new RuntimeException("Not implemented");
-  }
-
-  // TODO: You can add additional method within the range if you like
-
-  // --end-->
 }
