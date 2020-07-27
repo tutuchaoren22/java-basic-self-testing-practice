f6b059e31b756ab8b6da5b9d419ebfae5bb8e383
duyuqian <809009017@qq.com>
2020-07-27 17:27:58 +0800

    [duyuqian] feat-2: finish bool opearate

--- src/test/java/com/twc/javaBasic/BooleanOperatorsTest.java
+++ src/test/java/com/twc/javaBasic/BooleanOperatorsTest.java
@@ -3,36 +3,31 @@ package com.twc.javaBasic;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BooleanOperatorsTest {
-  @SuppressWarnings({"PointlessBooleanExpression", "ConstantConditions"})
-  @Test
-  void should_perform_logical_boolean_operations() {
-    boolean[] actualResults = {
-      true && true,
-      true && false,
-      false && false,
-      true || true,
-      true || false,
-      false || false,
-      true & true,
-      true & false,
-      false & false,
-      true | true,
-      true | false,
-      false | false,
-      3 == 7,
-      4 != 5
-    };
+    @SuppressWarnings({"PointlessBooleanExpression", "ConstantConditions"})
+    @Test
+    void should_perform_logical_boolean_operations() {
+        boolean[] actualResults = {
+                true && true,
+                true && false,
+                false && false,
+                true || true,
+                true || false,
+                false || false,
+                true & true,
+                true & false,
+                false & false,
+                true | true,
+                true | false,
+                false | false,
+                3 == 7,
+                4 != 5
+        };
 
-    // TODO:
-    //  please write your answers directly(modify the value of expectedResult to equal actualResults).
-    //
-    // <--start
-    boolean[] expectedResult = {};
-    // --end-->
+        boolean[] expectedResult = {true, false, false, true, true, false, true, false, false, true, true, false, false, true};
 
-    assertArrayEquals(expectedResult, actualResults);
-  }
+
+        assertArrayEquals(expectedResult, actualResults);
+    }
 }
